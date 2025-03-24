3720430e22aa31606ef9488ed0c9b4e552ae3a49
Kev-Ose <kevinosegueda@gmail.com>
2025-03-24 11:47:19 +0100

    modified hp style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,76 +1,70 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaFacebookF, FaInstagram, FaTiktok, FaWhatsapp } from 'react-icons/fa';
-import { WhatsappShareButton } from 'react-share';
 import './Home.css'; // Import your CSS file
-import lyv from '../../img/LyV Image.png'; // Replace with the correct path to your lyv image
+import granada from '../../img/granada.jpg'; // Hero background image
+import logo from '../../img/LyV Image.png'; // Restaurant logo
+import calzada from '../../img/calzadaGranada.jpg';
 
 const Homepage = () => {
     const location = useLocation();
-    const url = `localhost:5173${location.pathname}`; // Updated URL
-    const title = 'Check out this website!'; // Updated title
+    const url = `localhost:5173${location.pathname}`;
 
     return (
-        <section className="background-holder">
-            <div className="homepage-section">
-                <img src={lyv} alt="lyv" />
-                <div className="container">
-                    <button className="oswald-uniquifier">
-                        <a href="/menu" className="button-link">
-                            View Menu
-                        </a>
-                    </button>
-                    <button className="oswald-uniquifier">
-                        <a href="/reservations" className="button-link">
-                            Make a reservation
-                        </a>
-                    </button>
-                    <button className="oswald-uniquifier">
-                        <a href="/findus" className="button-link">
-                            Find us
-                        </a>
-                    </button>
+        <section className="homepage-section">
+            <div className="background-holder" />
+            <div className="homepage-components">
+                <img
+                    src={logo}
+                    alt="Restaurant Logo"
+                    className="restaurant-logo"
+                />
+
+                <div className="cta-buttons">
+                    <a href="/menu" className="cta-button">
+                        View Menu
+                    </a>
+                    <a href="/reservations" className="cta-button">
+                        Make a Reservation
+                    </a>
+                    <a href="/findus" className="cta-button">
+                        Find us
+                    </a>
                 </div>
-                <div className="social">
+
+                <div className="social-icons">
                     <Link
-                        className="icons"
-                        to={
-                            'https://www.facebook.com/profile.php?id=61550874188064'
-                        }
+                        to="https://www.facebook.com/profile.php?id=61550874188064"
                         target="_blank"
                         rel="noopener noreferrer"
+                        className="social-icon"
                     >
                         <FaFacebookF />
                     </Link>
                     <Link
-                        className="icons"
-                        to={
-                            'https://www.instagram.com/lagosyvolcanesbxl?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
-                        }
+                        to="https://www.instagram.com/lagosyvolcanesbxl?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
                         target="_blank"
                         rel="noopener noreferrer"
+                        className="social-icon"
                     >
                         <FaInstagram />
                     </Link>
                     <Link
-                        className="icons"
-                        to={
-                            'https://www.tiktok.com/@lagosyvolcanesbxl?is_from_webapp=1&sender_device=pc'
-                        }
+                        to="https://www.tiktok.com/@lagosyvolcanesbxl?is_from_webapp=1&sender_device=pc"
                         target="_blank"
                         rel="noopener noreferrer"
+                        className="social-icon"
                     >
                         <FaTiktok />
                     </Link>
-                    <div className="icons">
-                        <a
-                            href={`https://wa.me/${32498600048}?text=${encodeURIComponent("Hello, I would like to get information over")}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FaWhatsapp className='fawha' round={true} />
-                        </a>
-                    </div>
+                    <Link
+                        to={`https://wa.me/32498600048?text=${encodeURIComponent('')}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="social-icon"
+                    >
+                        <FaWhatsapp />
+                    </Link>
                 </div>
             </div>
         </section>
