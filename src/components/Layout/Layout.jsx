6956ca680e31b560b83b1d1763531daa8615f1a7
Kev-Ose <kevinosegueda@gmail.com>
2025-07-13 17:32:57 +0200

    removing analytics from layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { Analytics } from "@vercel/analytics/next"
 
 // import PropTypes from 'prop-types'
 
@@ -11,7 +10,6 @@ const Layout = () => {
             <Header />
             <Outlet />
             <Footer />
-            <Analytics />
         </>
     );
 };
